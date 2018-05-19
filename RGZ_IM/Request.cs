99679171ec550c4de49505728d41c3ab46542ad8
Luzyanin Alexey <261-96@mail.ru>
2018-05-19 12:26:30 +0700

    Class Request updated but I'm not sure that it'll work

--- RGZ_IM/Request.cs
+++ RGZ_IM/Request.cs
@@ -8,5 +8,35 @@ namespace RGZ_IM
 {
     public class Request
     {
+        Random random = new Random();
+
+        /// <summary>
+        /// Генерация интервала прихода заявок
+        /// </summary>
+        /// <returns></returns>
+        public double GetInterval() => 0.5 + PlusMinus();
+
+        /// <summary>
+        /// Генерация заявок
+        /// </summary>
+        /// <returns></returns>
+        public int GetRequest() => random.Next(0, 6);
+
+        /// <summary>
+        /// Уйдёт ли заявка?
+        /// 0, 1, 2, 3, 4, 5, 6, 7, 8 - проходит в очередь, 9 - отказ
+        /// </summary>
+        /// <returns></returns>
+        public int IsRequestDenied() => random.Next(0, 9);
+
+        /// <summary>
+        /// Пояснений не требуется
+        /// </summary>
+        /// <returns></returns>
+        double PlusMinus()
+        {
+            if (random.Next(0, 1) == 0) return random.NextDouble() / 3;
+            else return -random.NextDouble() / 3;
+        }
     }
 }
