3e0d3ae64de9479d5d8a5553211b140b90be0d7c
Алексей Гардер <aleksey_garder@outlook.com>
2018-05-19 17:46:46 +0700

    Add comments

--- RGZ_IM/SimaulationUtility.cs
+++ RGZ_IM/SimaulationUtility.cs
@@ -10,26 +10,46 @@ namespace RGZ_IM
     {
         readonly static Random random = new Random();
 
+        /// <summary>
+        /// Время прихода следующей заявки
+        /// </summary>
+        /// <param name="isWave">Режим волны</param>
         public static double GetNextPeopleTime(bool isWave)
         {
             throw new NotImplementedException();
         }
 
+
+        /// <summary>
+        /// Количество заявок, пришедших в очередь за 1 раз
+        /// </summary>
+        /// <param name="isWave">Режим волны</param>
         public static int GetNextPeopleCount(bool isWave)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Время до возникновения следующей волны
+        /// </summary>
         public static double GetWave()
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Продолжительность волны
+        /// </summary>
+        /// <returns></returns>
         public static double GetWaveLength()
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Время обслуживания заявки
+        /// </summary>
+        /// <returns></returns>
         public static double GetServiceTime()
         {
             throw new NotImplementedException();
