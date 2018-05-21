df1e27d5ae8b04ed9810c2397ab2b98a6491ccba
Luzyanin Alexey <261-96@mail.ru>
2018-05-21 19:11:05 +0700

    little iterface, OPEN the statistic data

--- RGZ_IM/Form1.cs
+++ RGZ_IM/Form1.cs
@@ -16,5 +16,10 @@ namespace RGZ_IM
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var statistic = Simulation.Simulate(Convert.ToDouble(FullTimeTextBox.Text));
+        }
     }
 }
