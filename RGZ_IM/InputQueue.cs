73f595e38d1b2b81fcfd529741a888cfb0b30f08
Алексей Гардер <aleksey_garder@outlook.com>
2018-05-26 17:04:43 +0700

    Rework channels statistic

---
+++ RGZ_IM/InputQueue.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace RGZ_IM
+{
+    class InputQueue : IQuest
+    {
+        private Wave wave;
+        private FlowAlterantive flow;
+
+        private readonly Queue<Statistic.Human> queue = new Queue<Statistic.Human>();
+
+        public int QueueLength => queue.Count;
+
+        public void Init(Wave wave, FlowAlterantive flow)
+        {
+            this.wave = wave;
+            this.flow = flow;
+        }
+
+        public InputQueue()
+        {
+            EndTime = SimulationUtility.GetNextPeopleTime(false);
+        }
+
+        public double EndTime { get; private set; }
+
+        public int CompareTo(IQuest other) => EndTime.CompareTo(other.EndTime);
+
+        public bool TryMake(double timeScale)
+        {
+            EndTime = timeScale + SimulationUtility.GetNextPeopleTime(wave.IsWave);
+            AddPeople(timeScale, SimulationUtility.GetNextPeopleCount(wave.IsWave));
+            flow.ToWork(timeScale);
+            return true;
+        }
+
+        public Statistic.Human Dequeue() => queue.Dequeue();
+
+        private void AddPeople(double timeScale, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                var orderTime = SimulationUtility.GetOrderTime();
+                var serviceTime = SimulationUtility.GetServiceTime(timeScale);
+
+                queue.Enqueue(new Statistic.Human(timeScale, orderTime, serviceTime));
+            }
+        }
+    }
+}
---
+++ RGZ_IM/Statistic.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RGZ_IM
+{
+    class Statistic
+    {
+
+        private double phase1Time;
+        private double phase2Time;
+        private double inQueueTime;
+
+        public int PeopleCount { get; private set; }
+
+        public IReadOnlyCollection<IReadOnlyChannel> Flows { get; set; }
+
+        public double MiddlePhase1Time => phase1Time / PeopleCount;
+
+        public double MiddlePhase2Time => phase2Time / PeopleCount;
+
+        public double MiddleInQueueTime => inQueueTime / PeopleCount;
+ 
+
+        public Statistic()
+        {
+            phase1Time = 0.0;
+            phase2Time = 0.0;
+            inQueueTime = 0.0;
+        }
+
+        public void AddStatistic(Human human)
+        {
+            var inQueue = human.EndTime - human.CreateTime - human.ServiceTimePhase1;
+
+            PeopleCount++;
+            inQueueTime += inQueue;
+            phase1Time += human.ServiceTimePhase1;
+            phase2Time += human.ServiceTimePhase2;
+        }
+
+        public interface IReadOnlyChannel
+        {
+            double InWorkTime { get; }
+
+            double ActiveTime { get; }
+        }
+
+        public class Channel : IReadOnlyChannel, IComparable<Channel>
+        {
+            public Human humanData = null;
+
+            public bool IsActive { get; private set; } = false;
+
+            public bool IsWork => humanData != null;
+
+            public double InWorkTime { get; private set; } = 0.0;
+
+            public double ActiveTime { get; private set; } = 0.0;
+
+            public double EndTime => humanData == null ? Double.MaxValue : humanData.EndTime;
+
+            public void PushHuman(Human data)
+            {
+                if ((humanData != null)||(!IsActive))
+                    throw new Exception("Incorrect change object");
+
+                humanData = data;   
+            }
+
+            public Human PopHuman()
+            {
+                if (humanData == null)
+                    throw new Exception("Incorrect change object");
+
+                var pool = humanData;
+                humanData = null;
+                return pool;
+            }
+
+            public void UpdateStatus(double deltaTime, bool isActive)
+            {
+                if (!IsWork) IsActive = isActive;
+                if (IsActive) ActiveTime += deltaTime;
+                if (IsWork) InWorkTime += deltaTime;
+            }
+
+            public int CompareTo(Channel other) => EndTime.CompareTo(other.EndTime);
+        }
+
+
+
+        public class Human:IComparable<Human>
+        {
+            public double CreateTime { get; private set; }
+
+            public double ServiceTimePhase1 { get; private set; }
+
+            public double ServiceTimePhase2 { get; private set; }
+
+            public double EndTime { get; private set; }
+
+            public void SetEndTime(double timeScale)
+            {
+                EndTime = timeScale + ServiceTimePhase1;
+            }
+
+            public int CompareTo(Human other)
+            {
+                return EndTime.CompareTo(other.EndTime);
+            }
+
+            public Human(double createTime, double serviceTimePhase1, double serviceTimePhase2)
+            {
+                CreateTime = createTime;
+                ServiceTimePhase1 = serviceTimePhase1;
+                ServiceTimePhase2 = serviceTimePhase2;
+            }
+        }
+    }
+}
