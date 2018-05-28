ccffcc2d5cbf175dedfb183a717d0818a110c6e5
Luzyanin Alexey <261-96@mail.ru>
2018-05-28 22:50:51 +0700

    interface update

--- RGZ_IM/Form1.cs
+++ RGZ_IM/Form1.cs
@@ -25,6 +25,7 @@ namespace RGZ_IM
             utility.WaveChannelCount = Convert.ToInt32(channelCountWaveTextBox.Text);
             utility.ExtremalChannelCount = Convert.ToInt32(channelExtremeTextBox.Text);
             int peopleCount = 0;
+            double fullPeopleCount = 0;
             double MiddleInQueue = 0;
             double MiddleInPhase1Time = 0;
             double MiddleInPhase2Time = 0;
@@ -37,6 +38,7 @@ namespace RGZ_IM
             for (int i = 0; i < itCount; i++)
             {
                 var statistic = Simulation.Simulate(Convert.ToDouble(FullTimeTextBox.Text), utility);
+                fullPeopleCount += statistic.FullPeopleCount;
                 peopleCount += statistic.PeopleCount;
                 MiddleInQueue += statistic.MiddleInQueueTime;
                 MiddleInPhase1Time += statistic.MiddlePhase1Time;
@@ -60,7 +62,7 @@ namespace RGZ_IM
             MiddleTimeInPhase1TextBox.Text = Math.Round((MiddleInPhase1Time / itCount), 2).ToString();
             MiddleTimeInPhase2TextBox.Text = Math.Round((MiddleInPhase2Time / itCount), 2).ToString();
             ChannelsMiddleTimeWorkTextBox.Text = Math.Round((InWorkTime / itCount), 2).ToString();
-            // ChannelMiddleTimeTextBox.Text = Math.Round((ActiveTime / itCount), 2).ToString();
+            MiddleRequestTextBox.Text = Math.Round((fullPeopleCount / (itCount * Convert.ToDouble(FullTimeTextBox.Text))), 2).ToString();
         }
 
         double WriteData(SimulationUtility utility)
